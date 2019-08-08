871bf2da43e4c8e113fce1b3213aed7d21130908
wkotheimer <wkotheimer@gmail.com>
2019-08-07 21:07:11 -0500

    First Commit

--- src/module1/FirstJavaClass.java
+++ src/module1/FirstJavaClass.java
@@ -8,6 +8,7 @@ public class FirstJavaClass {
 
 	public static void main(String[] args) {
 		
+		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\Selenium 3.4\\chromedriver_win32.chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
